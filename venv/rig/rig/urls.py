e0b37fd67fcd2edb945e88d674c92b3bed0d137f
rahulkrishnan221 <rahulkrishnan221@gmail.com>
2018-07-28 19:26:20 +0530

    Fixes

--- venv/rig/rig/urls.py
+++ venv/rig/rig/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rigapp/', include('rigapp.urls')),
+    url(r'^$', include('rigapp.urls')),
 ]
