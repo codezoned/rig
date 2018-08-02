1cb01d043e3eaef44963b6a9fa750a4a151e100d
Senthil Kumar <senthil1997@gmail.com>
2018-08-02 21:12:38 +0530

    added script id.py
    added id.py that creates qr pdf
    
    added csrf token in home.html

--- venv/rig/rigapp/urls.py
+++ venv/rig/rigapp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns =[
-    url(r'^$',views.index,name='index')]
+    url(r'^$',views.index,name='index'),
+    url(r'^submit',views.submit,name='submit'),]
 #r means regex start and ^ means start of line and $ is end of line
