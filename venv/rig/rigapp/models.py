b63fad43d704178bd86e104ea526cd7880c4a0c4
Senthil Kumar <senthil1997@gmail.com>
2018-08-25 18:35:16 +0530

    changes

--- venv/rig/rigapp/models.py
+++ venv/rig/rigapp/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+
+
+
+class Badge(models.Model):
+   name = models.CharField(max_length = 50)
+   picture = models.ImageField(upload_to = 'pictures')
